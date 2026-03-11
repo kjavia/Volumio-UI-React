41a49e8449df9e0c8596d99f9bddf67e44458c96
Ketan Javia <k_javia@hotmail.com>
2026-03-10 20:30:56 -0500

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-    return <></>
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-12">
+                    <h1>Volumio UI React</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
+import './index.scss'
 import App from './App.jsx'
 
 const queryClient = new QueryClient()
