6d5e1d7dbb80261cebddbbc5ea3dce53ad38474d
Ketan Javia <k_javia@hotmail.com>
2026-03-10 20:48:38 -0500

    Fixed styling and linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import js from '@eslint/js'
 import globals from 'globals'
+
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import prettier from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
+
+import js from '@eslint/js'
+import reactQuery from '@tanstack/eslint-plugin-query'
 
 export default [
     { ignores: ['dist'] },
@@ -18,16 +20,24 @@ export default [
                 sourceType: 'module',
             },
         },
+        settings: { react: { version: 'detect' } },
         plugins: {
+            react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
-            prettier,
+            ...reactQuery.configs['flat/recommended'],
         },
         rules: {
+            ...js.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
             ...reactHooks.configs.recommended.rules,
+            'react/jsx-no-target-blank': 'off',
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-            'prettier/prettier': 'warn',
+            'react/prop-types': 'off',
+            'react/display-name': 'off',
+            'react-hooks/set-state-in-effect': 'warn',
+            'react-hooks/refs': 'off',
         },
     },
-    prettierConfig,
 ]
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.scss'
-import App from './App.jsx'
-
-const queryClient = new QueryClient()
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.scss';
+import App from './App.jsx';
+import '../src/styles/themes/skeuomorphic.css';
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -12,4 +12,4 @@ createRoot(document.getElementById('root')).render(
             <App />
         </QueryClientProvider>
     </StrictMode>
-)
+);
