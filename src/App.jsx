4660f664ee03923a0739a291e852789c4361d549
Ketan Javia <k_javia@hotmail.com>
2026-03-11 22:47:39 -0500

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react';
 import Button from './components/Button.jsx';
 import VinylPlayer from './components/animated-players/VinylPlayer.jsx';
+import CdCoverPlayer from './components/animated-players/CdCoverPlayer.jsx';
+import CdPlayer from './components/animated-players/CdPlayer.jsx';
+import classNames from 'classnames';
 
 const App = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="container-fluid">
+      <h1 className="my-5">Volumio UI React</h1>
       <div className="row my-5">
-        <div className="col-12">
-          <h1 className="my-5">Volumio UI React</h1>
-          <VinylPlayer isPlaying={isPlaying} />
+        <div className="col-6">
+          <CdPlayer isPlaying={isPlaying} />
+        </div>
+        <div className="col-6">
+          <Button classNames="btn-square primary-text" onClick={() => setIsPlaying(!isPlaying)}>
+            <div className={classNames('led', { on: isPlaying })}></div>
+            {isPlaying ? 'Pause' : 'Play'}
+          </Button>
         </div>
       </div>
-      <Button onClick={() => setIsPlaying(!isPlaying)}>{isPlaying ? 'Pause' : 'Play'}</Button>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.scss';
 import App from './App.jsx';
 import '../src/styles/themes/skeuomorphic.css';
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App/>
+      <App />
     </QueryClientProvider>
   </StrictMode>
 );
