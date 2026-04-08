12043ac0d92a2e2492886c7f96f336a15b888362
KJavia <k_javia@hotmail.com>
2026-04-08 16:59:22 -0500

    Building Plugin for - Building Plugin for - Style fixes

--- src/hooks/usePluginConfig.js
+++ src/hooks/usePluginConfig.js
@@ -7,7 +7,7 @@ import { SocketContext } from '@/contexts/SocketContext';
 const fetchPluginConfig = async () => {
   const { data } = await axios.get(`${PLUGIN_BASE_URL}/api/config`);
   console.log('Fetched plugin config:', data);
-  return data;
+  return { ...data, albumArtMaxSpace: true };
 };
 
 const usePluginConfig = () => {
